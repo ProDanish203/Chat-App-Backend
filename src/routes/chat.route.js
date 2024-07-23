c3f7d090d83e4785e83daefab57b6c773721b35d
Danish <danishsidd524@gmail.com>
2024-07-23 17:41:16 +0500

    add: chat routes

--- src/routes/chat.route.js
+++ src/routes/chat.route.js
@@ -3,6 +3,11 @@ import {
     getChats,
     getMessages,
     sendMessage,
+    createGroup,
+    leaveGroup,
+    addMembers,
+    removeMembers,
+    updateGroup,
 } from "../controllers/chat.controller.js";
 import { verifyAuth } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -13,5 +18,11 @@ router.use(verifyAuth);
 router.get("/", getChats);
 router.get("/:chatId", getMessages);
 router.post("/:chatId", upload.array("attachments", 5), sendMessage);
+// For groups
+router.post("/group", createGroup);
+router.put("/group/:id", updateGroup);
+router.put("/group/:id/leave", leaveGroup);
+router.put("/group/:id/members/add", addMembers);
+router.put("/group/:id/members/remove", removeMembers);
 
 export default router;
